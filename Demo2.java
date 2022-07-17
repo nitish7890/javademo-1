5d2e9e7ec237f4a4a1a866b443e77c6856ebcc94
nitish7890 <nitishmehta1997@gmail.com>
2022-07-17 11:21:08 +0530

    some changes in demo2 file

--- Demo2.java
+++ Demo2.java
@@ -1,6 +1,7 @@
 public class Demo2 {
     public static void main(String args[]){
         System.out.println("Welcome in java second Program");
+        System.out.println("WElcome in nitish kumar mehta");
     }
 }
 
