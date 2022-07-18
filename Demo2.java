da3e7d1e2125d26accf8e75015d164138253f1d0
nitish7890 <82252897+nitish7890@users.noreply.github.com>
2022-07-18 17:09:57 +0530

    Delete Demo2.java

--- Demo2.java
+++
@@ -1,7 +0,0 @@
-public class Demo2 {
-    public static void main(String args[]){
-        System.out.println("Welcome in java second Program");
-        System.out.println("WElcome in nitish kumar mehta");
-    }
-}
-
