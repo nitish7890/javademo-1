2ac2030d4c3c302935c5ebb68d545c8756f3c47e
nitish7890 <82252897+nitish7890@users.noreply.github.com>
2022-07-18 17:31:03 +0530

    Update Demo1.java

--- Demo1.java
+++ Demo1.java
@@ -1,5 +1,5 @@
 public class Demo1{
     public static void main(String args[]){
-        System.out.println("Welcome in java Program");
+        System.out.println("Welcome in java Program in india");
     }
 }
